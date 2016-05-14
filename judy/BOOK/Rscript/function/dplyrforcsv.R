1a6c101b2bb9b10f26d120836541b010154c1681
howard40116 <howard40116@hotmail.com.tw>
2016-05-14 23:38:38 +0800

    0514
    judt test

---
+++ judy/BOOK/Rscript/function/dplyrforcsv.R
@@ -0,0 +1,24 @@
+﻿library(dplyr)
+#設定路徑　
+setwd("E:/book-recommender/judy/BOOK")
+
+
+x<-readLines("./input/book.csv",encoding = "UTF-8")
+
+
+
+x.header<-strsplit(x[1],split = ",")%>%
+  unlist()%>%
+  gsub("\"","",.)%>%
+  gsub("\\\\","",.)
+
+
+for(i in (2:length(x))){
+  x.header<-rbind(x.header,strsplit(x[i],split = ",")%>%unlist())
+}
+
+View(x.header)
+
+gsub("\"","",.)%>%
+  gsub("\\\\","",.)
+
