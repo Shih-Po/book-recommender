7dcdf9a83fb530f3eab8c6c893d0d5da65fde688
howard40116 <howard40116@hotmail.com.tw>
2016-05-17 16:29:30 +0800

    0517

--- judy/BOOK/Rscript/function/dplyrforcsv.R
+++ judy/BOOK/Rscript/function/dplyrforcsv.R
@@ -1,24 +1,25 @@
-﻿library(dplyr)
+library(dplyr)
 #設定路徑　
 setwd("E:/book-recommender/judy/BOOK")
-
-
 x<-readLines("./input/book.csv",encoding = "UTF-8")
 
-
-
-x.header<-strsplit(x[1],split = ",")%>%
-  unlist()%>%
-  gsub("\"","",.)%>%
-  gsub("\\\\","",.)
-
-
+x.header<-NULL
 for(i in (2:length(x))){
   x.header<-rbind(x.header,strsplit(x[i],split = ",")%>%unlist())
 }
 
-View(x.header)
-
-gsub("\"","",.)%>%
+x.sub<-
+  x.header%>%
+  gsub("\"","",.)%>%
   gsub("\\\\","",.)
+
+colnames(x.sub)<-  
+  strsplit(x[1],split = ",")%>%
+  unlist()%>%
+  gsub("\"","",.)%>%
+  gsub("\\\\","",.)
+
+db<-x.sub
+db%>%na.exclude()%>%View()
+db[3454,]
 
---
+++ judy/progress.md
@@ -0,0 +1,4 @@
+# book-recommender 進度
+
+
+
